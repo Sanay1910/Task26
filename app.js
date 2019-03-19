0f5160219d9b4490018ccc079ec72ab5cf375a4f
Sanay <sanay1910@gmail.com>
2019-03-19 12:00:15 +0100

    API calls kinda made

--- app.js
+++ app.js
@@ -1,88 +1,30 @@
-const express = require('express');
-const app = express;
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
 
-app.use('/user', (req, res) => {
-    res.render();
-})
-
-app.get('/user/id', (req, res => {
-  res
-}))
-
-app.post('/user/create', (req, res) => {
-  res
-})
-
-app.use('/restaurant', (req, res) => {
-    res.render();
-})
-
-app.use('/review', (res, req) => {
-  res.render();
-})
-
-port = process.env.port || 8080;
-
-app.listen(port);
-
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-const getUserId = (req, res) => {
-  const id = parseInt(req.params.id)
-
-  client.query('SELECT * user WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).json(results.row);
-  }) 
-}
-
-const createUser = (req, res) => {
-  const { username, email, password, role } = req.body
-
-  client.query('INSERT INTO user (username, email, password, role) VALUES ($1, $2, $3, $4)', [username, email, password, role], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(201).send('User added with ID:  ${result.insertId}')
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
-}
+)
 
-const updateUser = (req, res) => {
-  const id = parseInt(req.params.id)
-  const {username, email, password, role} = req.body
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-  client.query(
-    'UPDATE user SET username = $1, email=$2, password= $3, role = $4', 
-    [username, email, password, role],
-    (error, results) => {
-      if(error) {
-        throw error;
-      }
-      res.status(200).send('User modified with ID: ${id}')
-    }
+app.get('/user', db.getUsers)
 
-  )
-}
+app.get('/user/:id', db.getUserId)
 
-module.exports = {
-  getUserId,
-  createUser,
-  updateUser
-}
+app.post('/user', db.createUser)
+
+app.put('/user/:id', db.updateUser)
+
+app.get('/restaurant', db.getRestaurant)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
