a4e53af73e111ff0043469f34d3dccfa3ff15b54
Sanay <sanay1910@gmail.com>
2019-03-19 10:30:06 +0100

    test

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ app.use('/user', (req, res) => {
     res.render();
 })
 
-app.use('/(restaurant', (req, res) => {
+app.get('/user/id', (req, res => {
+  res
+}))
+
+app.post('/user/create', (req, res) => {
+  res
+})
+
+app.use('/restaurant', (req, res) => {
     res.render();
 })
+
+app.use('/review', (res, req) => {
+  res.render();
+})
+
+port = process.env.port || 8080;
+
+app.listen(port);
 
 const { Client } = require('pg');
 
@@ -25,3 +41,48 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   }
   client.end();
 });
+
+const getUserId = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  client.query('SELECT * user WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.row);
+  }) 
+}
+
+const createUser = (req, res) => {
+  const { username, email, password, role } = req.body
+
+  client.query('INSERT INTO user (username, email, password, role) VALUES ($1, $2, $3, $4)', [username, email, password, role], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(201).send('User added with ID:  ${result.insertId}')
+  })
+}
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id)
+  const {username, email, password, role} = req.body
+
+  client.query(
+    'UPDATE user SET username = $1, email=$2, password= $3, role = $4', 
+    [username, email, password, role],
+    (error, results) => {
+      if(error) {
+        throw error;
+      }
+      res.status(200).send('User modified with ID: ${id}')
+    }
+
+  )
+}
+
+module.exports = {
+  getUserId,
+  createUser,
+  updateUser
+}
