e360d9903aaabaa76ca57603e51207822eb1692d
Kim Aasen <kim.andre.aasen@gmail.com>
2019-03-20 09:28:49 +0100

    Added react files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// SKAL VÆRE HER
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
+
+// SKAL VÆRE HER
+ReactDOM.render(
+  <Main/>,
+  document.getElementById("root")
+);
