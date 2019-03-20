9bec764d743c5ea8f05d0718d1a7796890ce5c45
Sanay <sanay1910@gmail.com>
2019-03-20 15:01:45 +0100

    Ahhhhhh

--- apis/restaurantQueries.js
+++ apis/restaurantQueries.js
@@ -58,7 +58,7 @@ const getRestaurant = (req, res) => {
   }
 
   const getRestaurantByCategory = (req, res) => {
-    const category = String(req.params.category)
+    const category = req.body.category
 
     client.query('SELECT * FROM restaurant WHERE category = $1',[category], (error, results) => {
       if (error) {
@@ -69,7 +69,7 @@ const getRestaurant = (req, res) => {
   }
 
   const getRestaurantByName = (req, res) => {
-    const name = String(req.params.name)
+    const name = req.body.name
   
     client.query('SELECT * FROM restaurant WHERE name = $1',[name], (error, results) => {
       if (error) {
