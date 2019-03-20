cb7c0e48b007f8e74efecdba037bd3887250733f
Sanay <sanay1910@gmail.com>
2019-03-20 14:31:12 +0100

    Mashala

--- apis/restaurantQueries.js
+++ apis/restaurantQueries.js
@@ -58,7 +58,7 @@ const getRestaurant = (req, res) => {
   }
 
   const getRestaurantByCategory = (req, res) => {
-    const {category} = req.body
+    const {category} = req.category
 
     client.query('SELECT * FROM restaurant WHERE category = $1',[category], (error, results) => {
       if (error) {
@@ -69,7 +69,7 @@ const getRestaurant = (req, res) => {
   }
 
   const getRestaurantByName = (req, res) => {
-    const {name} = req.body
+    const {name} = req.name
   
     client.query('SELECT * FROM restaurant WHERE name = $1',[name], (error, results) => {
       if (error) {
